5ac20a3a0fa7edc504756d5b43ff5465a8e7b095
montgomeryrd <montgomery_rd@yahoo.com>
2020-11-25 22:24:52 -0800

    created schemas, created a db in mongoDB Atlas, retrieves data from DB

--- server.js
+++ server.js
@@ -2,27 +2,44 @@ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 const gql = require('graphql-tag');
 
+const Post = require('./models/Post.js');
 const { MONGODB } = require('./config.js');
 
+// Type definitions and Queries
 const typeDefs = gql`
-    type Query {
-        sayHi: String!
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
     }
-`
+    type Query {
+        getPosts: [Post]
+    }
+`;
 // Resolvers for each Query
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch(err) {
+                throw new Error(err);
+            }
+        }
     }
-}
+};
 
+// Creating server
 const server = new ApolloServer({
     typeDefs,
     resolvers
-})
+});
 
 const PORT = 3000 || process.env.PORT;
 
+// useNewUrlParser depracated, added useUnifiedTopology
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB Connected');
